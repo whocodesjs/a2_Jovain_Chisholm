e3c897ca0cd210d71375424520ea178d629789bf
Jovain Chisholm <whocodesjs@gmail.com>
2025-02-17 18:34:53 -0500

    optimized code for functionality, added show all button for markers, added the filter function to filter by location type.

--- js/map.js
+++ js/map.js
@@ -9,36 +9,73 @@ function initMap() {
     mapId: "MAP_ID_GOES_HERE", // Remove or replace with a valid Map ID
   });
 
-  let infoWindow = new google.maps.InfoWindow();
-
-  let marker_clicked = function () {
-    infoWindow.close();
-    infoWindow.setContent(this.NAME);
-    infoWindow.open(map, this);
-  };
-
-  for (i = 0; i < locations.length; i++) {
-    if (locations[i].type === "park")
+  for (const location of locations) {
+    let new_icon;
+    if (location.type === "park")
       new_icon = "https://maps.google.com/mapfiles/ms/icons/green-dot.png";
-    else if (locations[i].type === "museum")
+    else if (location.type === "museum")
       new_icon = "https://maps.google.com/mapfiles/ms/icons/blue-dot.png";
-    else if (locations[i].type === "waterfall")
-      new_icon = "https://maps.google.com/mapfiles/ms/icons/waterfall.png";
-    else if (locations[i].type === "restaurant")
+    else if (location.type === "waterfall")
+      new_icon = "https://maps.google.com/mapfiles/ms/icons/yellow-dot.png";
+    else if (location.type === "restaurant")
       new_icon = "https://maps.google.com/mapfiles/ms/icons/purple-dot.png";
     else new_icon = "https://maps.google.com/mapfiles/ms/icons/red-dot.png";
 
     const icon_content = document.createElement("img");
     icon_content.src = new_icon;
 
-    new_marker = new google.maps.marker.AdvancedMarkerElement({
+    const contentString = `<h3>${location.name}</h3>${
+      location.address ? `<p>${location.address}</p>` : ""
+    }`;
+
+    const infoWindow = new google.maps.InfoWindow({
+      content: contentString,
+    });
+
+    const marker = new google.maps.marker.AdvancedMarkerElement({
       map: map,
-      position: { lat: locations[i].lat, lng: locations[i].lng },
-      title: locations[i].name,
+      position: { lat: location.lat, lng: location.lng },
+      title: location.name,
       content: icon_content,
     });
 
-    new_marker.NAME = locations[i].name;
-    new_marker.addListener("click", marker_clicked);
+    marker.type = location.type;
+
+    marker.addListener("click", () => {
+      infoWindow.close();
+      infoWindow.open({
+        anchor: marker,
+        map,
+        shouldFocus: false,
+      });
+    });
+
+    markers.push(marker);
   }
+
+  document
+    .getElementById("show-parks")
+    .addEventListener("click", () => filterMarkers("park"));
+  document
+    .getElementById("show-museums")
+    .addEventListener("click", () => filterMarkers("museum"));
+  document
+    .getElementById("show-waterfalls")
+    .addEventListener("click", () => filterMarkers("waterfall"));
+  document
+    .getElementById("show-restaurants")
+    .addEventListener("click", () => filterMarkers("restaurant"));
+
+  document.getElementById("show-all").addEventListener("click", () => {
+    markers.forEach((marker) => {
+      marker.map = map;
+    });
+  });
+}
+
+// Filter markers by type
+function filterMarkers(type) {
+  markers.forEach((marker) => {
+    marker.map = marker.type === type ? map : null;
+  });
 }
