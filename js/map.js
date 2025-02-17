5e7a39172e732c7912e20cce3a8b56d0e3941508
Jovain Chisholm <whocodesjs@gmail.com>
2025-02-16 22:18:16 -0500

    updated coordinates for markers that were added.

--- js/map.js
+++ js/map.js
@@ -8,4 +8,37 @@ function initMap() {
     zoom: 12,
     mapId: "MAP_ID_GOES_HERE", // Remove or replace with a valid Map ID
   });
+
+  let infoWindow = new google.maps.InfoWindow();
+
+  let marker_clicked = function () {
+    infoWindow.close();
+    infoWindow.setContent(this.NAME);
+    infoWindow.open(map, this);
+  };
+
+  for (i = 0; i < locations.length; i++) {
+    if (locations[i].type === "park")
+      new_icon = "https://maps.google.com/mapfiles/ms/icons/green-dot.png";
+    else if (locations[i].type === "museum")
+      new_icon = "https://maps.google.com/mapfiles/ms/icons/blue-dot.png";
+    else if (locations[i].type === "waterfall")
+      new_icon = "https://maps.google.com/mapfiles/ms/icons/waterfall.png";
+    else if (locations[i].type === "restaurant")
+      new_icon = "https://maps.google.com/mapfiles/ms/icons/purple-dot.png";
+    else new_icon = "https://maps.google.com/mapfiles/ms/icons/red-dot.png";
+
+    const icon_content = document.createElement("img");
+    icon_content.src = new_icon;
+
+    new_marker = new google.maps.marker.AdvancedMarkerElement({
+      map: map,
+      position: { lat: locations[i].lat, lng: locations[i].lng },
+      title: locations[i].name,
+      content: icon_content,
+    });
+
+    new_marker.NAME = locations[i].name;
+    new_marker.addListener("click", marker_clicked);
+  }
 }
